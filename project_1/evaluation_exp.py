656d15046acb0c7cf0cd22ec41da90093c5f7e4f
tomanci <80816847+tomanci@users.noreply.github.com>
2023-10-16 18:43:45 +0200

    modifed rules for project

--- project_1/evaluation_exp.py
+++ project_1/evaluation_exp.py
@@ -10,11 +10,11 @@ class EvalExpr(lark.Transformer):
         return args[0]*args[1]
     
     
-source = '2*((2+1)*2)+(2+3)*2'
+source = "sum(int x,int y){return x + y;}"
 
 print(">> Input string: ",source)
 
-json_parser = lark.Lark.open("expressions.lark",rel_to=__file__,start="expr",parser='lalr')
+json_parser = lark.Lark.open("functions_rule.lark",rel_to=__file__,start="function_name",parser='lalr')
 result = json_parser.parse(source)
 print("\n*** Parse tree pretty print\n", result.pretty())
 
@@ -22,5 +22,5 @@ print("\n*** Parse tree pretty print\n", result.pretty())
 #graph = lark.tree.pydot__tree_to_graph(result, "TB")
 #graph.write_pdf("expr_tree.pdf")
 
-eval_tree = EvalExpr().transform(result)
-print(">> Result: ", eval_tree)
+#eval_tree = EvalExpr().transform(result)
+#print(">> Result: ", eval_tree)
