da09cbd4b2fe15fbb8fd950feeddd865c8f1ecc0
tomanci <80816847+tomanci@users.noreply.github.com>
2023-10-21 18:28:44 +0200

    grammar done, starting transformer

--- project_1/evaluation_exp.py
+++ project_1/evaluation_exp.py
@@ -2,30 +2,45 @@ import lark
 
 class FuncionDefinition(lark.Transformer):
     def __init__(self):
-        return
+        self.param_list = []
+        self.oper_list = [] 
 
-    NUMBER = float
+    def fun_definition(self,operation):
+        self.param_list = [operation]
+        self.oper_list = [operation]
+        return self.oper_list
 
-    def sum(self,args):
-        return args[0]+args[1]
+    #NUMBER = float
+
+    def sum(self,*args):
+        print(args)
+        return args[0]
     
     def mul(self,args):
-        return args[0]*args[1]
+        return args[0] * args[1]
     
     
-source = """fun_name(x,y,z)
-            {return x + y * z;}"""
+source = """sum(x,y)
+            {return x + y;}"""
+
 
 print(">> Input string: ",source)
 
-json_parser = lark.Lark.open("functions_rule.lark", rel_to = __file__, start = "function_name",
-                             parser = 'lalr')#, transformer = FuncionDefinition())
-result = json_parser.parse(source)
+function_parser = lark.Lark.open("functions_rule.lark", rel_to = __file__, start = "function_name",
+                             parser = 'lalr', transformer = FuncionDefinition())
+result = function_parser.parse(source)
 print("\n*** Parse tree pretty print\n", result.pretty())
 
-# print tree to PDF file
-#graph = lark.tree.pydot__tree_to_graph(result, "TB")
-#graph.write_pdf("expr_tree.pdf")
+"""
+def main():
+    source = input("> ")
+    function_parser = lark.Lark.open("functions_rule.lark", rel_to = __file__, start = "function_name",
+                             parser = 'lalr', transformer = FuncionDefinition())
+    result = function_parser.parse(source)
+    print("\n*** Parse tree pretty print\n", result.pretty())
 
-#eval_tree = EvalExpr().transform(result)
-#print(">> Result: ", eval_tree)
+
+if __name__ == '__main__':
+    main()
+
+"""
