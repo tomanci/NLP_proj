218c997b14ec09b51807f50966cc841e69da16cc
tomanci <80816847+tomanci@users.noreply.github.com>
2023-10-22 22:57:43 +0200

    small improvements

--- project_1/evaluation_exp.py
+++ project_1/evaluation_exp.py
@@ -1,16 +1,33 @@
 import lark
 
 class FuncionDefinition(lark.Transformer):
+    
     def __init__(self):
         self.param_list = []
         self.oper_list = [] 
-
-    def fun_definition(self,operation):
-        self.param_list = [operation]
+    """
+    def fun_definition(self, parameter, operation):
+        self.param_list = [parameter]
         self.oper_list = [operation]
+        x = self.oper_list[0]
+        for i in x:
+            print(i) 
         return self.oper_list
+    """
 
-    #NUMBER = float
+    def fun_definition(self,parameter):
+        self.param_list = [parameter]
+        return self.param_list
+    
+    def oper_definition(self, operation):
+        self.oper_list = [operation]
+        print(self.oper_list[0])
+        return self.oper_list
+    #NUMBER = float  
+    
+    def __check_number_parameters(self):
+        if len(self.param_list[0]) != len(self.oper_list[0]):
+            raise ValueError ("the number of parameters does not correspond!!!")
 
     def sum(self,*args):
         print(args)
@@ -20,10 +37,10 @@ class FuncionDefinition(lark.Transformer):
         return args[0] * args[1]
     
     
-source = """sum(x,y)
-            {return x + y;}"""
+source = """f_n(x,y)
+            {return x_1 + y_1*z;}"""
 
-
+#source = "f_n(x,y)"
 print(">> Input string: ",source)
 
 function_parser = lark.Lark.open("functions_rule.lark", rel_to = __file__, start = "function_name",
