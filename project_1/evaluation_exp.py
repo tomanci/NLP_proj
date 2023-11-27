9baa16c244833568cc43ae6bcdddca36445242c7
tomanci <80816847+tomanci@users.noreply.github.com>
2023-11-28 00:05:21 +0100

    add comments to project 1

--- project_1/evaluation_exp.py
+++ project_1/evaluation_exp.py
@@ -11,13 +11,13 @@ class FuncionDefinition(lark.Transformer):
    
     def fun_initialization_def(self,values):
         self.def_fun_name = values[0]
-        self.param_list = values[1]
-        self.oper_list = values[2]
+        self.param_list = values[1] #parameter
+        self.oper_list = values[2] #operand 
         self._check_name(self.oper_list,self.param_list)
         
     def fun_initialization_call(self,values):
         self.call_fun_name = values[0]
-        self.parm_call_list = values[1]
+        self.parm_call_list = values[1] #expression like 1*5+6*4
         self._check_number_parameters(self.parm_call_list, self.param_list, self.oper_list)
         self._check_name_function(self.def_fun_name, self.call_fun_name)
         result = self.evaluation(self.oper_list, self.param_list ,self.parm_call_list)
@@ -32,6 +32,7 @@ class FuncionDefinition(lark.Transformer):
         self.parm_call_list = [parameter]
         return self.parm_call_list
     
+    #check if the number of parameter in the function call and definition match
     def _check_number_parameters(self,list_param_def,list_parm_call, oper_list):
 
         flat_oper_list = self.flat(oper_list) 
@@ -41,6 +42,7 @@ class FuncionDefinition(lark.Transformer):
         if len(list_parm_call[0]) != len(flat_oper_list) or len(list_param_def[0]) != len(flat_oper_list):
             raise ValueError ("the number of INPUT paramters and OUTPUT parameters do not correspond!!!")
                
+    # check if the name is the same 
     def _check_name_function(self, string_1, string_2):
         if string_1 != string_2:
             raise ValueError ("the names of the function do not correspond!!!")
@@ -65,19 +67,21 @@ class FuncionDefinition(lark.Transformer):
     
     def evaluation(self, expression, param_list, parm_call_list):
         operation = expression[0]
-    
+        #check its length: if == 1 -> we do not need to call a recursive function    
         if len(expression[1:]) == 1:
             operands = expression[1]
         else:
             operands = expression[1:]
 
         if isinstance(operands, list):
-            operands = [self.evaluation(operand, param_list ,parm_call_list) 
+            operands = [self.evaluation(operand, param_list, parm_call_list) 
                         if isinstance(operand, list) else operand for operand in operands]
 
         return self.lazy_evaluate(operation, operands, self.param_list ,self.parm_call_list)
 
     def lazy_evaluate(self, operation, operands, param_list ,parm_call_list):
+
+        #compute the actual partial result of two operands
 
         operand1 = operands[0]
         operand2 = operands[1]
@@ -91,7 +95,9 @@ class FuncionDefinition(lark.Transformer):
             return operand1 * operand2
         else:
             return operand1 + operand2
-    
+
+    #match the variable with its value, by checking where the variable is called in the function.
+    #I use operand and param_list, seeing where the corrispondence exists. Once that i retrive the value
     def _check_correspondence(self, operand, param_list, param_call_list):
         counter = 0
         for item in param_list[0]: 
